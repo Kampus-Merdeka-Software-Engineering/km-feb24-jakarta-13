f8200abe513ccecde02d67ab8c6c7bbedcae7f05
Viraaa <118715206+rizkamagvira@users.noreply.github.com>
2024-05-15 23:14:09 +0700

    Add files via upload

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -1,68 +1,90 @@
-        
-        // Js untuk Bulan
-        const ctxBulan = document.getElementById('myChartBulan').getContext('2d');
-            new Chart(ctxBulan, {
-                type: 'line', // tipe chart
-                data: {
-                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                    datasets: [{
-                        label: 'Total Sales',
-                        data: [65, 59, 80, 81, 56, 55, 40],
-                        borderColor: 'rgb(75, 192, 192)',
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
-                }
-            });
+// For toggling and finding number of children and other stuff is done here!
 
-        // Js untuk Tahun
-        // ctx adalah sebuah variabel. itu nanti akan digunakan untuk menggambar grafik menggunakan Chart.js.
-        // document.getElementById('myChart'); untuk memanggil nama id yang ada di tag canvas
-        const ctx = document.getElementById('myChart'); 
-      
-        new Chart(ctx, {
-          type: 'bar',
-          data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              borderWidth: 1
-            }]
-          },
-          options: {
-            scales: {
-              y: {
-                beginAtZero: true
-              }
-            }
-          }
-        });
+const navigation = document.getElementById("nav");
+const menu = document.getElementById("menu");
 
-        // Js untuk State
-        const ctxr = document.getElementById('myChartState'); 
-      
-        new Chart(ctxr, {
-          type: 'scatter',
-          data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              borderWidth: 1
-            }]
-          },
-          options: {
-            scales: {
-              y: {
-                beginAtZero: true
-              }
-            }
-          }
-        });
+menu.addEventListener("click", () => {
+  // The navigation.children.length means the following :-
+  // The children inside a parent are basically an array of elements; So, here I'm finding the length of the array aka how many children are inside the nav bar.
+  //   Yup That's all.
+  navigation.style.setProperty("--childenNumber", navigation.children.length);
+
+  //   Casually Toggling Classes to make them animate on click
+  //   Regular stuff ;)
+  navigation.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+
+// Js untuk Bulan
+const ctxBulan = document.getElementById("myChartBulan").getContext("2d");
+new Chart(ctxBulan, {
+  type: "line", // tipe chart
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Total Sales",
+        data: [65, 59, 80, 81, 56, 55, 40],
+        borderColor: "rgb(75, 192, 192)",
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+// Js untuk Tahun
+// ctx adalah sebuah variabel. itu nanti akan digunakan untuk menggambar grafik menggunakan Chart.js.
+// document.getElementById('myChart'); untuk memanggil nama id yang ada di tag canvas
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+// Js untuk State
+const ctxr = document.getElementById("myChartState");
+
+new Chart(ctxr, {
+  type: "scatter",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
